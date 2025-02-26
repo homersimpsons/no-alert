eb17ee717879f23745cd467d1aee9882e05339b9
Guillaume <guillaume.alabre@gmail.com>
2025-02-26 01:06:05 +0100

    :construction_worker: Add CI for lint/test/publish
    Also run fmt command

--- no-alert.ts
+++ no-alert.ts
@@ -1,22 +1,25 @@
 const plugin: Deno.lint.Plugin = {
-    name: "no-alert",
-    rules: {
-        "no-alert": {
-            create(context) {
-                return {
-                    CallExpression(node) {
-                        const callee = node.callee;
-                        if (callee.type === "Identifier" && ["alert", "confirm", "prompt"].includes(callee.name)) {
-                            context.report({
-                                node,
-                                message: `Unexpected call to \`${callee.name}\``
-                            })
-                        }
-                    }
-                }
+  name: "no-alert",
+  rules: {
+    "no-alert": {
+      create(context) {
+        return {
+          CallExpression(node) {
+            const callee = node.callee;
+            if (
+              callee.type === "Identifier" &&
+              ["alert", "confirm", "prompt"].includes(callee.name)
+            ) {
+              context.report({
+                node,
+                message: `Unexpected call to \`${callee.name}\``,
+              });
             }
-        }
-    }
-}
+          },
+        };
+      },
+    },
+  },
+};
 
 export default plugin;
--- no-alert_test.ts
+++ no-alert_test.ts
@@ -2,30 +2,32 @@ import { assertEquals } from "jsr:@std/assert";
 import plugin from "./no-alert.ts";
 
 Deno.test("no-alert", () => {
-    const diagnostics = Deno.lint.runPlugin(plugin, "main.ts", "alert('test');");
+  const diagnostics = Deno.lint.runPlugin(plugin, "main.ts", "alert('test');");
 
-    assertEquals(diagnostics.length, 1);
-    const d = diagnostics[0];
-    assertEquals(d.id, "no-alert/no-alert");
-    assertEquals(d.message, "Unexpected call to `alert`");
-})
-
+  assertEquals(diagnostics.length, 1);
+  const d = diagnostics[0];
+  assertEquals(d.id, "no-alert/no-alert");
+  assertEquals(d.message, "Unexpected call to `alert`");
+});
 
 Deno.test("no-confirm", () => {
-    const diagnostics = Deno.lint.runPlugin(plugin, "main.ts", "confirm('test');");
+  const diagnostics = Deno.lint.runPlugin(
+    plugin,
+    "main.ts",
+    "confirm('test');",
+  );
 
-    assertEquals(diagnostics.length, 1);
-    const d = diagnostics[0];
-    assertEquals(d.id, "no-alert/no-alert");
-    assertEquals(d.message, "Unexpected call to `confirm`");
-})
-
+  assertEquals(diagnostics.length, 1);
+  const d = diagnostics[0];
+  assertEquals(d.id, "no-alert/no-alert");
+  assertEquals(d.message, "Unexpected call to `confirm`");
+});
 
 Deno.test("no-prompt", () => {
-    const diagnostics = Deno.lint.runPlugin(plugin, "main.ts", "prompt('test');");
+  const diagnostics = Deno.lint.runPlugin(plugin, "main.ts", "prompt('test');");
 
-    assertEquals(diagnostics.length, 1);
-    const d = diagnostics[0];
-    assertEquals(d.id, "no-alert/no-alert");
-    assertEquals(d.message, "Unexpected call to `prompt`");
-})
+  assertEquals(diagnostics.length, 1);
+  const d = diagnostics[0];
+  assertEquals(d.id, "no-alert/no-alert");
+  assertEquals(d.message, "Unexpected call to `prompt`");
+});
